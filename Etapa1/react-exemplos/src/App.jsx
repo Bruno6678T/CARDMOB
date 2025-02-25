d285df3604019406b01cf7f6d5893d44e9845d14
Coimbra <Coimbrab176@gmail.com>
2025-02-25 10:19:43 -0300

    revisão React 1

---
+++ Etapa1/react-exemplos/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Counter from './components/Counter'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  // function updateCount() {
+  //  setCount(count+1)
+  // }
+  
+  // arrow function
+
+  const updateCount = () => {
+    // outros comandos
+    return count + 1;
+  }
+  
+  const updateCount1 = () => count + 1; // return é implicito
+
+  const dados = {
+    "nome": "fulano",
+    "atualiza": (novo_nome) => `Nome nome é $(novo_nome)`,
+    "endereco": {
+      "rua": "xyz",
+      "numero": "111",
+      "complementos": ["casa", "na esquina do supermercado ABC"]
+    }
+  }; // é um objeto JS
+  dados.atualiza("gerson")
+  dados.endereco.complementos[1] // acessando a refêrencia do endereço
+  
+
+  return (
+    <>
+      <Counter title="Meu ovo"/>
+      <Counter initial="8000"/>
+    </>
+  )
+}
+
+export default App
