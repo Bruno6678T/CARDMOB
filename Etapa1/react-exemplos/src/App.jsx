b26ba96ef7ae780e5f7b9418e53674a3308aef5f
Bruno Coimbra <coimbrab176@gmail.com>
2025-03-11 10:07:44 -0300

    feat

--- Etapa1/react-exemplos/src/App.jsx
+++ Etapa1/react-exemplos/src/App.jsx
@@ -1,18 +1,51 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 import Counter from './components/Counter';
+import Photo from './components/Photo';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchPhotos = async () => {
+    try {
+      const url = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+      const response = await fetch(url); // Por padrão executa um request do tipo GET
+      // console.log(response);
+      if (response.status === 200) {
+        const data = await response.json();
+      console.log(data);
+      // correção da imagem thumb
+      const updatePhotos = data.map( (photo) => ({
+        ...photo,
+        thumbnailUrl: `https://picsum.photos/150?random=${photo.id}`
+      }));
+      // ...photo { id: 1, title: "rótulo", thumbnailUrl: "http:///",...}
+      // { photo: { id: 1, title: "rotulo",... }}
+        setPhotos(updatePhotos);
+      }
+
+
+    } catch (error) {
+      consoloe.error('Erro ao buscar fotos', error);
+    }
+
+  }
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
 
   // function updateCount() {
   //   setCount(count+1);
   // }
 
   // arraw function 
+  // () => () // return implícito
+  // () => {} // é necessário colocar o return 
 
   const updateCount = () => {
     // outros comandos
@@ -37,8 +70,19 @@ function App() {
 
   return (
     <>
-      <Counter title="Contando..." />
-      <Counter initial="100" />
+      <Counter title="Contador" />
+      <Counter initial="8000" />
+      <article>
+        <h1>Album da API</h1>
+        {photos.map( (photo) => (
+        //  <article key={photo.id}>
+        //     <h2>ID #{photo.id} {photo.title}</h2>
+        //     <img src={photo.thumbnailUrl} alt={photo.title} />
+        //  </article>
+        <Photo photo={photo} />
+        ))}
+      </article>
+      
     </>
   )
 }
