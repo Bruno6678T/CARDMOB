bae5b83051c2f3e4b71593450da8ab8f81c57bed
Bruno Coimbra <coimbrab176@gmail.com>
2025-08-12 08:43:25 -0300

    Inclui ThemeContext

--- Etapa3/loja-react-app/App.tsx
+++ Etapa3/loja-react-app/App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { ThemeProvider } from './src/contexts/ThemeContext'; // NOVA
 import HomeScreen from './src/screens/HomeSreen';
 
 export default function App() {
   return (
+      <ThemeProvider>
       <HomeScreen />
+      </ThemeProvider>
   );
 }
 
