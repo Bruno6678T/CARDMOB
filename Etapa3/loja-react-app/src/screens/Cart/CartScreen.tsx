e757cc43dde869e37148231d952b1e9903aa889e
Bruno Coimbra <coimbrab176@gmail.com>
2025-11-04 08:14:51 -0300

    Concluir carrinho de compras

--- Etapa3/loja-react-app/src/screens/Cart/CartScreen.tsx
+++ Etapa3/loja-react-app/src/screens/Cart/CartScreen.tsx
@@ -6,7 +6,7 @@ import CartItem from './CartItem';
 import { useShop } from '../../contexts/ShopContext';
 
 const CartScreen = ({ navigation }: any) => {
-    const { cartItems } = useShop();
+    const { cartItems, getTotalPrice, clearCart } = useShop();
 
     const renderItem = ({item} : any) => (
         <CartItem item={item} />
@@ -18,44 +18,46 @@ const CartScreen = ({ navigation }: any) => {
 
     return (
         <View style={styles.container}>
-            {cartItems.length === 0} ? (
-                <Text style={styles.empty}>Seu carrinho está vazio.</Text>
-                <Button 
-                    title='Ver produtos'
-                    onPress={ () => navigation.navigate('Catalog') }
-                />
+            {cartItems.length === 0 ? (
+                <View style={styles.container}>
+                    <Text style={styles.empty}>Seu carrinho está vazio.</Text>
+                    <Button 
+                        title='Ver produtos'
+                        onPress={ () => navigation.navigate('Catalog') }
+                    />
+                </View>
             ) : (
-        <View style={styles.listContainer}>
-            <Text>Carrinho de compras</Text>
-            <FlatList 
-                data={cartItems}
-                renderItem={renderItem}
-                keyExtractor={(item: any) => item.id.toString()}
-            />
-            <View style={styles.totalContainer}>
-                    <Text style={styles.totalText}>Total R$ {handleCheckout}</Text>
-                    <TouchableOpacity 
-                    onPress={handleCheckout}
-                    style={styles.clearButton}
-                    >
-                        <Text style={styles.clearButtonText}>Limpar carrinho</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => navigation.navite('Catalog')}
-                        style={styles.continueButton}
-                    >
-                            <Text style={styles.clearButtonText}>Continuar comprando</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate('Catalog')}
-                        style={styles.checkoutButton}
-                    >
-                            <Text style={styles.checkoutButtonText}>Concluir pedido</Text>
-                    </TouchableOpacity>
-            </View>
-        </View>
-            
+                <View style={styles.listContainer}>
+                    {/* <Text>Carrinho de compras</Text> */}
+                    <FlatList 
+                        data={cartItems}
+                        renderItem={renderItem}
+                        keyExtractor={(item: any) => item.id.toString()}
+                    />
+                    <View style={styles.totalContainer}>
+                        <Text style={styles.totalText}>Total R$ {getTotalPrice().toString()}</Text>
+                        <TouchableOpacity
+                            onPress={clearCart}
+                            style={styles.clearButton}
+                        >
+                            <Text style={styles.clearButtonText}>Limpar carrinho</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('Catalog')}
+                            style={styles.continueButton}
+                        >
+                            <Text style={styles.continueButtonText}>Continuar comprando</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('Catalog')}
+                            style={styles.checkoutButton}
+                        >
+                            <Text style={styles.checkoutButtonText}>Concluir Pedido</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             )
+            }
         </View>
     );
 };
@@ -117,4 +119,4 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 16,
     },
-})
+});
